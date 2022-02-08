10fd2886928b5aac1fbe02c0f7832515757c26d6
Maksim Nasalevich <maksim_nasalevich@epam.com>
2022-02-08 14:59:10 +0300

    Cprt-9: Splash screen UI
    - created splash screen

--- shared/feature/splash/src/commonMain/kotlin/com/cprt/advancedauction/splash/presentation/SplashScreenUI.kt
+++ shared/feature/splash/src/commonMain/kotlin/com/cprt/advancedauction/splash/presentation/SplashScreenUI.kt
@@ -1,13 +1,44 @@
 package com.cprt.advancedauction.splash.presentation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.cprt.advancedauction.core.screen.tools.SplashScreen
+import com.cprt.advancedauction.theme.icons.AAIcons
+import com.cprt.advancedauction.theme.icons.aaicons.IcCopartLogo
 
 class SplashScreenUI : SplashScreen {
 
     @Composable
     override fun Content() {
-        Text("SplashScreenUI")
+        val navigator = LocalNavigator.currentOrThrow
+
+        Surface {
+            Button(
+                onClick = {
+                    navigator.pop()
+                }
+            ) {
+                Text("pop")
+            }
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Image(
+                    modifier = Modifier.size(160.dp),
+                    contentDescription = null,
+                    imageVector = AAIcons.IcCopartLogo,
+                )
+            }
+        }
     }
 }
