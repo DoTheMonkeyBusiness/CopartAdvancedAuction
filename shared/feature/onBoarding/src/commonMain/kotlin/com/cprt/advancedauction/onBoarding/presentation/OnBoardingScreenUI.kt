1dd4118e87a2fc2390e74f1c07af08b904cea063
Maksim Nasalevich <maksim_nasalevich@epam.com>
2022-04-14 11:58:29 +0300

    Cprt-28: Vehicle search screen UI
    - Added import

--- shared/feature/onBoarding/src/commonMain/kotlin/com/cprt/advancedauction/onBoarding/presentation/OnBoardingScreenUI.kt
+++ shared/feature/onBoarding/src/commonMain/kotlin/com/cprt/advancedauction/onBoarding/presentation/OnBoardingScreenUI.kt
@@ -2,8 +2,9 @@ package com.cprt.advancedauction.onBoarding.presentation
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.cprt.advancedauction.navigation.tools.OnBoardingScreen
 
-class OnBoardingScreenUI : com.cprt.advancedauction.navigation.tools.OnBoardingScreen {
+class OnBoardingScreenUI : OnBoardingScreen {
 
     @Composable
     override fun Content() {
